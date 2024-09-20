5722bd329acc2525726a0c0fe2bd7649d5df077c
Pauline Oliveira <apaulineoliveira@gmail.com>
2024-09-20 16:17:09 -0300

    Refactor (reestruturando código js)

--- src/script.js
+++ src/script.js
@@ -1,30 +1,22 @@
+
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
-
-const months = [
-  "janeiro",
-  "fevereiro",
-  "março",
-  "abril",
-  "maio",
-  "junho",
-  "julho",
-  "agosto",
-  "setembro",
-  "outubro",
-  "novembro",
-  "dezembro",
-];
-
 const closeBox = popupBox.querySelector("header i");
 const titleTag = popupBox.querySelector("input");
 const descTag = popupBox.querySelector("textarea");
 const addBtn = popupBox.querySelector("button");
+const menuel = document.querySelector(".iconel");
+
+
+const months = [
+  "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+  "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+];
+
+
 
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 const lastId = parseInt(localStorage.getItem("lastId") || "0");
-
-const menuel = document.querySelector(".iconel");
 
 
 const getNextId = () => {
@@ -34,90 +26,105 @@ const getNextId = () => {
   return newId;
 };
 
+
 const showNotes = () => {
   document.querySelectorAll(".note").forEach((note) => note.remove());
+
+  
   notes.sort((a, b) => b.id - a.id);
 
 
   notes.forEach((note, index) => {
-    let litag = `<li class="note">
-    
-                            <div class="details">
-                                <p> ${note.title} </p>
-                                <span>${note.description}</span>
-                            </div>
-
-                            <div class="bottom-content">
-
-                                <span>${note.date}</span>
-                                  <div class="settings">
-                                      <i onclick=showMenu(this) class="fa-solid fa-gear"></i>
-
-                                      <ul class="menu">
-                                          <li onclick="editNote(${index},'${note.title}','${note.description}')"><i class="fa-solid fa-pencil"></i>EDITAR</li>
-                                          <li onclick="deleteNote(${index})"><i class="fa-solid fa-trash"></i>EXCLUIR</li>
-                                      </ul>
-                                  </div>
-                            </div>
-                     </li>`;
-
+    const litag = `
+      <li class="note">
+        <div class="details">
+          <p>${note.title}</p>
+          <span>${note.description}</span>
+        </div>
+        <div class="bottom-content">
+          <span>${note.date}</span>
+          <div class="settings">
+            <i onclick="showMenu(this)" class="fa-solid fa-gear"></i>
+            <ul class="menu">
+              <li onclick="editNote(${index}, '${note.title}', '${note.description}')">
+                <i class="fa-solid fa-pencil"></i> EDITAR
+              </li>
+              <li onclick="deleteNote(${index})">
+                <i class="fa-solid fa-trash"></i> EXCLUIR
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>`;
     addBox.insertAdjacentHTML("afterend", litag);
   });
 };
 
+
 function showMenu(elem) {
   elem.parentElement.classList.add("show");
   document.onclick = (e) => {
-    if (e.target.tagName != "I" || e.target != elem) {
+    if (e.target.tagName !== "I" || e.target !== elem) {
       elem.parentElement.classList.remove("show");
     }
   };
 }
 
+
 function deleteNote(noteId) {
-  notes.splice(noteId, 1);
-  localStorage.setItem("notes", JSON.stringify(notes));
-  showNotes();
+  notes.splice(noteId, 1); 
+  localStorage.setItem("notes", JSON.stringify(notes)); 
+  showNotes(); 
 }
+
 
 function editNote(noteId, title, description) {
   titleTag.value = title;
   descTag.value = description;
-  addBox.click();
-  deleteNote(noteId);
+  addBox.click(); 
+  deleteNote(noteId); 
 }
 
+
 addBox.onclick = () => popupBox.classList.add("show");
+
+
 closeBox.onclick = () => {
   titleTag.value = "";
   descTag.value = "";
   popupBox.classList.remove("show");
 };
 
+
 addBtn.onclick = (e) => {
-  e.preventDefault();
+  e.preventDefault(); 
 
-  let ti = titleTag.value;
-  let desc = descTag.value;
+  const title = titleTag.value;
+  const description = descTag.value;
 
-  let currentDate = new Date();
-  let month = months[currentDate.getMonth()];
-  let day = currentDate.getDate();
-  let year = currentDate.getFullYear();
 
-  let noteInfo = {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = months[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+
+
+
+  const noteInfo = {
     id: getNextId(),
-    title: ti,
-    description: desc,
+    title: title,
+    description: description,
     date: `${day} ${month} ${year}`,
   };
 
-  notes.push(noteInfo);
 
+  notes.push(noteInfo);
   localStorage.setItem("notes", JSON.stringify(notes));
+
 
   closeBox.click();
   showNotes();
 };
+
 
 showNotes();
