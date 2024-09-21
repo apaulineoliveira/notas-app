e847c2de8ff8cb5cfea18ad0cc4cb7017320c26a
Pauline Oliveira <apaulineoliveira@gmail.com>
2024-09-21 12:30:29 -0300

    Feature (src/script.js) : Implementa condição para impedir salvamento de nota vazia, altera remoção de nota atual ao editar nota

--- src/script.js
+++ src/script.js
@@ -7,6 +7,9 @@ const descTag = popupBox.querySelector("textarea");
 const addBtn = popupBox.querySelector("button");
 const menuel = document.querySelector(".iconel");
 
+let isEditing = false;
+let editIndex = null;
+
 
 const months = [
   "janeiro", "fevereiro", "março", "abril", "maio", "junho",
@@ -39,7 +42,7 @@ const showNotes = () => {
       <li class="note">
         <div class="details">
           <p>${note.title}</p>
-          <span>${note.description}</span>
+          <span>${note.description.length > 100 ? note.description.slice(0, 100) + '...' : note.description}</span>
         </div>
         <div class="bottom-content">
           <span>${note.date}</span>
@@ -81,8 +84,9 @@ function deleteNote(noteId) {
 function editNote(noteId, title, description) {
   titleTag.value = title;
   descTag.value = description;
+  isEditing = true;
+  editIndex = noteId;
   addBox.click(); 
-  deleteNote(noteId); 
 }
 
 
@@ -103,6 +107,11 @@ addBtn.onclick = (e) => {
   const description = descTag.value;
 
 
+  if (!title || !description) {
+    alert("Por favor, preencha todos os campos antes de salvar.");
+    return;
+  }
+
   const currentDate = new Date();
   const day = currentDate.getDate();
   const month = months[currentDate.getMonth()];
@@ -111,14 +120,22 @@ addBtn.onclick = (e) => {
 
 
   const noteInfo = {
-    id: getNextId(),
+    id: isEditing ? notes[editIndex].id : getNextId(),
     title: title,
     description: description,
     date: `${day} ${month} ${year}`,
   };
+  
 
 
-  notes.push(noteInfo);
+  if (isEditing) {
+    notes[editIndex] = noteInfo;
+    isEditing = false;
+    editIndex = null;
+  } else {
+    notes.push(noteInfo);
+  }
+
   localStorage.setItem("notes", JSON.stringify(notes));
 
 
